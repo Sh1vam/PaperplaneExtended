f3c9bd69f4308b2596f5cd5e427f2a919447c475
Sh1vam <70308097+Sh1vam@users.noreply.github.com>
2020-11-11 14:35:26 +0530

    Add files via upload

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -1,64 +1,46 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-# The entire source code is OSSRPL except 'whois' which is MPL
-# License: MPL and OSSRPL
-""" Userbot module for getiing info about any user on Telegram(including you!). """
+"""Get Telegram User Information
+Syntax: .whois @username/userid"""
+
+
+
 
 import os
-
 from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.utils import get_input_location
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
-from telethon.utils import get_input_location
-from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
-from userbot.events import register
 
 
-@register(pattern="^\.whois(?: |$)(.*)", outgoing=True)
+from userbot.utils import admin_cmd
+
+TMP_DOWNLOAD_DIRECTORY = "./"
+
+
+
+@borg.on(admin_cmd(pattern="whois (.*)"))
 async def who(event):
+    if event.fwd_from:
+        return
 
-    await event.edit(
-        "`Sit tight while I steal some data from Mark Zuckerburg...`")
-
-    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+    
+    if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TMP_DOWNLOAD_DIRECTORY)
 
     replied_user = await get_user(event)
 
-    try:
-        photo, caption = await fetch_info(replied_user, event)
-    except AttributeError:
-        event.edit("`Could not fetch info of that user.`")
-        return
+    caption = await fetch_info(replied_user, event)
 
     message_id_to_reply = event.message.reply_to_msg_id
 
     if not message_id_to_reply:
         message_id_to_reply = None
 
-    try:
-        await event.client.send_file(event.chat_id,
-                                     photo,
-                                     caption=caption,
-                                     link_preview=False,
-                                     force_document=False,
-                                     reply_to=message_id_to_reply,
-                                     parse_mode="html")
-
-        if not photo.startswith("http"):
-            os.remove(photo)
-        await event.delete()
-
-    except TypeError:
-        await event.edit(caption, parse_mode="html")
+    await event.edit(caption, parse_mode="html")
 
 
 async def get_user(event):
     """ Get the user from argument or replied message. """
-    if event.reply_to_msg_id and not event.pattern_match.group(1):
+    if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         replied_user = await event.client(
             GetFullUserRequest(previous_message.from_id))
@@ -90,7 +72,6 @@ async def get_user(event):
 
     return replied_user
 
-
 async def fetch_info(replied_user, event):
     """ Get details from the User object. """
     replied_user_profile_photos = await event.client(
@@ -98,7 +79,7 @@ async def fetch_info(replied_user, event):
                              offset=42,
                              max_id=0,
                              limit=80))
-    replied_user_profile_photos_count = "Person needs help with uploading profile picture."
+    replied_user_profile_photos_count = "NaN."
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
     except AttributeError as e:
@@ -109,7 +90,7 @@ async def fetch_info(replied_user, event):
     try:
         dc_id, location = get_input_location(replied_user.profile_photo)
     except Exception as e:
-        dc_id = "Couldn't fetch DC ID!"
+        dc_id = "Need a Profile Picture to check DC ID!"
         location = str(e)
     common_chat = replied_user.common_chats_count
     username = replied_user.user.username
@@ -118,37 +99,45 @@ async def fetch_info(replied_user, event):
     restricted = replied_user.user.restricted
     verified = replied_user.user.verified
     photo = await event.client.download_profile_photo(user_id,
-                                                      TEMP_DOWNLOAD_DIRECTORY +
+                                        TMP_DOWNLOAD_DIRECTORY +
                                                       str(user_id) + ".jpg",
                                                       download_big=True)
     first_name = first_name.replace(
-        "\u2060", "") if first_name else ("This User has no First Name")
+        "\u2060", "") if first_name else ("")
     last_name = last_name.replace(
-        "\u2060", "") if last_name else ("This User has no Last Name")
+        "\u2060", "") if last_name else ("")
     username = "@{}".format(username) if username else (
         "This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
+    if user_id != (await event.client.get_me()).id:
+        common_chat = replied_user.common_chats_count
+    else:
+        common_chat = "I've seen them in... Wow. Are they stalking me? "
+        common_chat += "They're in all the same places I am... oh. It's me."
+        
+    
+    caption = "<b>General Info OF:</b> \n"
+    caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a> \n"
+    caption += f"<b>First Name</b>: {first_name} \n"
+    caption += f"<b>ID</b>: <code>{user_id}</code> \n"
+    caption += f"Last Name</b>: {last_name} \n"
+    caption += f"<b>Username</b>: {username} \n"
+    caption += f"DC ID: {dc_id}\n"
+    caption += f"Number of PPs: {replied_user_profile_photos_count}\n"
+    caption += f"Common Groups: {common_chat} \n \n"
+    caption += f"<b>Bio</b>: \n<code>{user_bio}</code> \n"
 
-    caption = "<b>USER INFO:</b>\n\n"
-    caption += f"First Name: {first_name}\n"
-    caption += f"Last Name: {last_name}\n"
-    caption += f"Username: {username}\n"
-    caption += f"Data Centre ID: {dc_id}\n"
-    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"Is Bot: {is_bot}\n"
-    caption += f"Is Restricted: {restricted}\n"
-    caption += f"Is Verified by Telegram: {verified}\n"
-    caption += f"ID: <code>{user_id}</code>\n\n"
-    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Common Chats with this user: {common_chat}\n"
-    caption += f"Permanent Link To Profile: "
-    caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
-
-    return photo, caption
+    return caption
+    
+    
+    
 
 
-CMD_HELP.update({
-    "whois":
-    ".whois <username> or reply to someones text with .whois\
-    \nUsage: Gets info of an user."
-})
+
+    
+    
+
+    
+    
+
+    
